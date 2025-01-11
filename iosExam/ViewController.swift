fc2e230d50cbac24d1625395736e0492c1da2aaf
lalsaghe <layla.alsagheer@gmail.com>
2025-01-12 00:35:41 +0300

    Initial Commit

---
+++ iosExam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosExam
+//
+//  Created by Layla Alsagheer on 12/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
